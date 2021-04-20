0dacac57eb22c20137e349d0bcc3512a790e92fa
Mattia Bocchino <pmattiab@live.it>
2021-04-20 15:40:31 +0200

    struttura

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            imgCorrente: 0,
+            immagini: [
+                "img/campagna.jpg",
+                "img/cascate.webp",
+                "img/fiori.jpeg",
+                "img/montagne.webp"
+            ]
+        },
+        methods: {
+
+        }
+        
+    }
+);
